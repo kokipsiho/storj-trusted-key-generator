b1ef01a1e13fb89b1df3d00a2d2e0fed07da422d
Zoran Joka <zoran.joka@gmail.com>
2018-03-07 19:26:23 +0100

    add format to output

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const HDKey = require('hdkey');
 const seed = require('crypto').randomBytes(64);
 const hdKey = HDKey.fromMasterSeed(seed);
 const complexKey = hdKey.derive("m/3000'/0'");
+const chalk = require ('chalk');
+const print = console.log;
 
-console.log('prv', complexKey.privateExtendedKey);
-console.log('pub', complexKey.publicExtendedKey);
+print(chalk.dim('-----------------------'));
+print(chalk.red.bold('PRIVATE EXTENDED KEY'), chalk.yellow.bold('->')) 
+print(chalk.green.bold(complexKey.privateExtendedKey));
+print(chalk.dim('Use this in renter config file'));
+print(chalk.dim('-----------------------'));
+print(chalk.red.bold('PUBLIC EXTENDED KEY'), chalk.yellow.bold('->')) 
+print(chalk.green.bold(complexKey.publicExtendedKey));
+print(chalk.dim('Use this as farmer trusted key'));
+print(chalk.dim('-----------------------'));
