ac6ee8ccf5af36d1b3e7437b469e73e1697e94f1
Zoran Joka <zoran.joka@gmail.com>
2018-03-26 14:49:59 +0200

    add file output

--- index.js
+++ index.js
@@ -3,14 +3,47 @@ const seed = require('crypto').randomBytes(64);
 const hdKey = HDKey.fromMasterSeed(seed);
 const complexKey = hdKey.derive("m/3000'/0'");
 const chalk = require ('chalk');
+const fs = require('fs');
 const print = console.log;
 
-print(chalk.dim('-----------------------'));
-print(chalk.red.bold('PRIVATE EXTENDED KEY'), chalk.yellow.bold('->')) 
-print(chalk.green.bold(complexKey.privateExtendedKey));
-print(chalk.dim('Use this in renter config file'));
-print(chalk.dim('-----------------------'));
-print(chalk.red.bold('PUBLIC EXTENDED KEY'), chalk.yellow.bold('->')) 
-print(chalk.green.bold(complexKey.publicExtendedKey));
-print(chalk.dim('Use this as farmer trusted key'));
-print(chalk.dim('-----------------------'));
+
+let outputFile = 'keys.output';
+let tempOutput;
+
+fs.unlink(outputFile, (err) => {
+    tempOutput = '-----------------------';
+    print(chalk.dim(tempOutput));
+    fs.appendFileSync(outputFile, tempOutput + '\n');
+    
+    tempOutput = 'PRIVATE EXTENDED KEY';
+    print(chalk.red.bold(tempOutput), chalk.yellow.bold('->')) ;
+    fs.appendFileSync(outputFile, tempOutput + '\n');
+
+    tempOutput = complexKey.privateExtendedKey;
+    print(chalk.green.bold(tempOutput));
+    fs.appendFileSync(outputFile, tempOutput + '\n');
+
+    tempOutput = 'Use this in renter config file';
+    print(chalk.dim(tempOutput));
+    fs.appendFileSync(outputFile, tempOutput + '\n');
+
+    tempOutput = '-----------------------';
+    print(chalk.dim(tempOutput));
+    fs.appendFileSync(outputFile, tempOutput + '\n');
+
+    tempOutput = 'PUBLIC EXTENDED KEY';
+    print(chalk.red.bold(tempOutput), chalk.yellow.bold('->')) ;
+    fs.appendFileSync(outputFile, tempOutput + '\n'); 
+
+    tempOutput = complexKey.publicExtendedKey;
+    print(chalk.green.bold(tempOutput));
+    fs.appendFileSync(outputFile, tempOutput + '\n');
+    
+    tempOutput = 'Use this as farmer trusted key';
+    print(chalk.dim(tempOutput));
+    fs.appendFileSync(outputFile, tempOutput + '\n');
+    
+    tempOutput = '-----------------------';
+    print(chalk.dim(tempOutput));
+    fs.appendFileSync(outputFile, tempOutput + '\n');
+})
